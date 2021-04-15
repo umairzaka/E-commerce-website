9ef49e446c240b13461d5ebdee68670add5e7f0c
umairzaka <umairzaka007@gmail.com>
2021-04-15 23:59:22 +0500

    a little bit change

--- backend/Data.js
+++ backend/Data.js
@@ -2,7 +2,7 @@ const data = {
     products: [
       {
         _id: '1',
-        name: 'Nike Slim Shirts',
+        name: 'Nike Slim Shirt',
         category: 'Shirts',
         image: '/images/p1.jpg',
         price: 120,
